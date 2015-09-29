d86f59d67d793f62bf2c74c120917f1005ead679
Paul McGroarty <paulmg310@gmail.com>
2015-09-29 13:49:28 +0100

    added favourite search
    search to generate table for jquery

---
+++ src/project/search/favouritesSearch.java
@@ -0,0 +1,49 @@
+package project.search;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import project.dal.Database;
+
+@Path("/favsearch")
+public class favouritesSearch 
+{
+	@GET
+	@Produces("text/plain")
+	public String getText() throws SQLException
+	{
+		Connection con = null;
+		String html = "";
+		try
+		{
+			con = Database.getConnection();
+			Statement st = con.createStatement();
+	   
+			ResultSet rs = st.executeQuery("SELECT f.stockSymbol, s.stockName FROM favourite f JOIN stock s ON s.stockSymbol = f.stockSymbol");
+			html += "<table>";
+			while(rs.next())
+			{
+				html += "<tr><td><a href='graphPage.jsp?sym="+ rs.getString("stockSymbol") + "'>" + rs.getString("stockName") + "</a></td></tr>";
+			}
+			html += "</table>";
+		}
+		catch (SQLException ex) 
+		{
+			System.out.println("Database error " + ex);
+		}
+		finally
+		{
+			if(con!=null)
+			{
+				con.close();
+			}
+		}
+		return html;
+	}
+}
