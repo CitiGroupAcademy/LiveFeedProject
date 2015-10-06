ef140f7d87f4af755d532b6f8b6058dfdfe03fd7
nwallwin01 <nwallwin01@qub.ac.uk>
2015-10-06 15:39:12 +0100

    removed unused imports

--- src/project/dal/GetQuotes.java
+++ src/project/dal/GetQuotes.java
@@ -4,14 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Queue;
-
 import org.jboss.logging.Logger;
 
 import project.dal.OrderManager.OrderResult;
