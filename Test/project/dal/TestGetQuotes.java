698a0c2200567fd536b5aa651bd47841795320f7
nwallwin01 <nwallwin01@qub.ac.uk>
2015-10-08 16:57:45 +0100

    testing junit

--- Test/project/dal/TestGetQuotes.java
+++ Test/project/dal/TestGetQuotes.java
@@ -32,5 +32,133 @@ public class TestGetQuotes {
 		}
 
 	}
+	
+	@Test
+	public void testMovingAverageBuy(){
+		DataAccess.insertStock("TEST.L", "Moving Average Test");
+		DataAccess.insertStrategy(1, "TEST.L", "movingAvg", "active");
+		DataAccess.updateStockChange("TEST.L", "", "", "", "","", 100, 120, 0);
+		
+		GetQuotes.moivngAverageStrategy();
+		
+		for(project.dataObjects.Transaction t :DataAccess.getTransaction()){
+			
+			if(t.getStockSymbol().equalsIgnoreCase("TEST.L")){
+				assertEquals(t.getAmount(), 0);
+			}
+		}
+		
+		
+		removeTestStock("TEST.L");
+		
+	}
+	
+	@Test
+	public void testMovingAverageSell(){
+		DataAccess.insertStock("TEST.L", "Moving Average Test");
+		DataAccess.insertStrategy(1, "TEST.L", "movingAvg", "active");
+		DataAccess.updateStockChange("TEST.L", "", "", "", "","", 120, 100, 0);
+		
+		GetQuotes.moivngAverageStrategy();
+		
+		for(project.dataObjects.Transaction t :DataAccess.getTransaction()){
+			
+			if(t.getStockSymbol().equalsIgnoreCase("TEST.L")){
+				assertEquals(t.getAmount(), -10);
+			}
+		}
+		
+		
+		removeTestStock("TEST.L");
+		
+	}
+	
+	@Test
+	public void testMovingAverageExpBuy(){
+		DataAccess.insertStock("TEST.L", "Moving Average Test");
+		DataAccess.insertStrategy(1, "TEST.L", "movAvgExp", "active");
+		DataAccess.updateStockChange("TEST.L", "", "", "", "","", 100, 120, 0);
+		
+		GetQuotes.moivngAverageStrategy();
+		
+		for(project.dataObjects.Transaction t :DataAccess.getTransaction()){
+			
+			if(t.getStockSymbol().equalsIgnoreCase("TEST.L")){
+				assertEquals(t.getAmount(), 20);
+			}
+		}
+		
+		
+		removeTestStock("TEST.L");
+		
+	}
+	
+	@Test
+	public void testMovingAverageExpSell(){
+		DataAccess.insertStock("TEST.L", "Moving Average Test");
+		DataAccess.insertStrategy(1, "TEST.L", "movAvgExp", "active");
+		DataAccess.updateStockChange("TEST.L", "", "", "", "","", 120, 100, 0);
+		
+		GetQuotes.moivngAverageStrategy();
+		
+		for(project.dataObjects.Transaction t :DataAccess.getTransaction()){
+			
+			if(t.getStockSymbol().equalsIgnoreCase("TEST.L")){
+				assertEquals(t.getAmount(), -20);
+			}
+		}
+		
+		
+		removeTestStock("TEST.L");
+		
+	}
+	
+	@Test
+	public void testMovingAverageBolBuy(){
+		DataAccess.insertStock("TEST.L", "Moving Average Test");
+		DataAccess.insertStrategy(1, "TEST.L", "bollinger", "active");
+		DataAccess.updateStockChange("TEST.L", "", "", "", "","", 98, 100, 1);
+		
+		GetQuotes.moivngAverageStrategy();
+		
+		for(project.dataObjects.Transaction t :DataAccess.getTransaction()){
+			
+			if(t.getStockSymbol().equalsIgnoreCase("TEST.L")){
+				assertEquals(t.getAmount(), 2);
+			}
+		}
+		
+		
+		removeTestStock("TEST.L");
+		
+	}
+	
+	@Test
+	public void testMovingAverageBolSell(){
+		DataAccess.insertStock("TEST.L", "Moving Average Test");
+		DataAccess.insertStrategy(1, "TEST.L", "bollinger", "active");
+		DataAccess.updateStockChange("TEST.L", "", "", "", "","", 102, 100, 1);
+		
+		GetQuotes.moivngAverageStrategy();
+		
+		for(project.dataObjects.Transaction t :DataAccess.getTransaction()){
+			
+			if(t.getStockSymbol().equalsIgnoreCase("TEST.L")){
+				assertEquals(t.getAmount(), 2);
+			}
+		}
+		
+		
+		removeTestStock("TEST.L");
+		
+	}
+	
+	public static void removeTestStock(String symbol){
+		DataAccess.removeFavourite(symbol);
+		DataAccess.removeStrategy(symbol);
+		DataAccess.removeStock(symbol);
+		DataAccess.removeTransaction(symbol);
+		DataAccess.removeOwnedStock(symbol);
+	}
 
 }
